57f03d41cd6e016d49b30dd20cee64a69f07dd5b
Rafal Plis <rafal.plis@gmail.com>
2025-10-10 18:00:52 +0200

    Update azurerm provider version to 4.40.0 across multiple modules for consistency and improved functionality.

--- modules/rg-logging/main.tf
+++ modules/rg-logging/main.tf
@@ -1,9 +1,10 @@
 terraform {
-required_providers {
-azurerm = {
-source = "hashicorp/azurerm"
-}
-}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
 }
 
 
